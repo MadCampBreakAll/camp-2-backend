d871ad1b35911c868fac100c916995b0dbf6d5a9
sjwhole <bluemoon2718@naver.com>
2022-01-07 18:10:31 +0900

    feat: Use middleware get me

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { loginUser, registerUser, getMe } from "../controllers/users.js";
+import { authChecker } from "../middleware/authChecker.js";
 
 let router = express.Router();
 
 router.post("/login", loginUser);
 router.post("/register", registerUser);
-router.get("/me", getMe);
+router.get("/me", authChecker, getMe);
 
 export default router;
