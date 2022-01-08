9fddcb02452593c6ab898d1036b1226543b8ccb9
sjwhole <bluemoon2718@naver.com>
2022-01-08 21:44:59 +0900

    feat: Add createAt to diaryInnerPages

--- src/utils/pages.js
+++ src/utils/pages.js
@@ -41,6 +41,7 @@ export const findDiaryInnerPages = (diaryId) => {
       body: true,
       color: true,
       img: true,
+      created_at: true,
       user: {
         select: {
           id: true,
