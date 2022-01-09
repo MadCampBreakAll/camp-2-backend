c2852664dd7d34da9435225c66a065ada04e6265
Seongjun Kim <66054861+sjwhole@users.noreply.github.com>
2022-01-09 18:40:44 +0900

    Feature/aws s3 (#10)
    * feat: Change authChecker status to login
    
    * feat: Add upload to s3

--- src/routes/pages.js
+++ src/routes/pages.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { createNewPage, getDiaryPages } from "../controllers/pages.js";
 import { authChecker } from "../middleware/authChecker.js";
-import { toAsyncRouter } from "../utils/errors.js";
+import { upload } from "../utils/image.js";
+import { wrapAsync } from "../utils/errors.js";
 
-let router = toAsyncRouter(express.Router());
+let router = express.Router();
 
-router.get("/", authChecker, getDiaryPages);
-router.post("/", authChecker, createNewPage);
+router.get("/", authChecker, wrapAsync(getDiaryPages));
+router.post("/", authChecker, upload.single("img"), wrapAsync(createNewPage));
 
 export default router;
